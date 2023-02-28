2579cfcac346bab1f4ce2396e4aa22cb09a92c22
Robert Fisher <fisher.robert26@gmail.com>
2023-02-28 15:13:10 -0600

    Initial AWS lambda setup for nodejs and python

---
+++ nodejs/discord-bot-channel-messages/index.js
@@ -0,0 +1,14 @@
+//AWS NodeJS Lambda Handler
+
+exports.handler = async (event, context) => {
+
+};
+
+
+//UNCOMMENT FOR TESTING:
+/* 
+var event
+exports.handler(event).then(result => {
+  console.log(result)
+});
+*/
